5bb08f72e9b0b046a9a1402e5e9644507c5f3c53
ramzi-saadeh <rsaadeh2012@gmail.com>
2026-03-26 09:07:32 +0200

    add capacitor

---
+++ android/app/src/main/java/com/bus/leb/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bus.leb;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bus.leb',
+  appName: 'bus-leb',
+  webDir: 'dist/bus-leb/browser'
+};
+
+export default config;
